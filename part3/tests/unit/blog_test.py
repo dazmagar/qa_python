b215992c007dcb678294358e206a701b883582dc
Dmitrii Sherstianykh <dsherstianykh@deloitte.com>
2020-06-18 02:14:32 +0300

    added app tests

--- part3/tests/unit/blog_test.py
+++ part3/tests/unit/blog_test.py
@@ -11,6 +11,22 @@ class BlogTest(TestCase):
         self.assertEqual(0, len(b.posts))
         self.assertListEqual([], b.posts)
 
+    def test_repr(self):
+        b = Blog('Test', 'Test Author')
+        b2 = Blog('My Day', 'Rolf')
+
+        self.assertEqual(b.__repr__(), 'Test by Test Author (0 posts)')
+        self.assertEqual(b2.__repr__(), 'My Day by Rolf (0 posts)')
+
+    def test_repr_multiple_posts(self):
+        b = Blog('Test', 'Test Author')
+        b.posts = ['test']
+        b2 = Blog('My Day', 'Rolf')
+        b2.posts = ['test', 'another']
+
+        self.assertEqual(b.__repr__(), 'Test by Test Author (1 post)')
+        self.assertEqual(b2.__repr__(), 'My Day by Rolf (2 posts)')
+
     def test_json_no_posts(self):
         b = Blog('Test', 'Test author')
 
